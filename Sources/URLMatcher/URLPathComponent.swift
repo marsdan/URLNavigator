ebe6c632d6705ef67bfeb4866ea905d7ff6f87ff
marsdan <z994489675@qq.com>
2023-12-05 10:31:14 +0800

    优化URL参数解析判定逻辑

--- Sources/URLMatcher/URLPathComponent.swift
+++ Sources/URLMatcher/URLPathComponent.swift
@@ -5,19 +5,8 @@ enum URLPathComponent {
 
 extension URLPathComponent {
   init(_ value: String) {
-      if value.hasPrefix("[") && value.hasSuffix("]") {
-          let start = value.index(after: value.startIndex)
-          let end = value.index(before: value.endIndex)
-          let placeholder = value[start..<end] // e.g. "<int:id>" -> "int:id"
-          let typeAndKey = placeholder.components(separatedBy: ":")
-          if typeAndKey.count == 1 { // any-type placeholder
-              self = .placeholder(type: nil, key: typeAndKey[0])
-          } else if typeAndKey.count == 2 {
-              self = .placeholder(type: typeAndKey[0], key: typeAndKey[1])
-          } else {
-              self = .plain(value)
-          }
-      } else if value.hasPrefix("<") && value.hasSuffix(">") {
+      if (value.hasPrefix("[") && value.hasSuffix("]")) ||
+            (value.hasPrefix("<") && value.hasSuffix(">")) {
           let start = value.index(after: value.startIndex)
           let end = value.index(before: value.endIndex)
           let placeholder = value[start..<end] // e.g. "<int:id>" -> "int:id"
